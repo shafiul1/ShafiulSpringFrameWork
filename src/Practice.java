0137b58a62e02b60d2762ee7d4d4ec98dc02c120
Shafiul Islam <shafiul.is@gmail.com>
2017-11-12 12:47:10 -0500

    2nd

--- src/Practice.java
+++ src/Practice.java
@@ -8,12 +8,13 @@
  */
 public class Practice {
 
-	/**
-	 * @param args
-	 */
+	 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int x= 10;
+		
+		String a="hello";
+		
 	}
 
 }
