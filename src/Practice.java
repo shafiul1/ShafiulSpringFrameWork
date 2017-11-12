a33a8ac4370f5dd5a4f72851f855a86d3583d3ef
Shafiul Islam <shafiul.is@gmail.com>
2017-11-12 12:38:16 -0500

    My First Commit

---
+++ src/Practice.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author shafiul
+ *
+ */
+public class Practice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
